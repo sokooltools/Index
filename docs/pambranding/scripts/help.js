ff12082c58a05e2f7b3e24983ad09559ef013128
SokoolTools <57449110+sokooltools@users.noreply.github.com>
2019-12-26 14:41:07 -0500

    Modified   docs/pambranding/main.htm Modified   docs/pambranding/scripts/help.js Modified   docs/pambranding/themes/word.css Modified   docs/pamtools/images/img-32.png Modified   docs/themes/help.css

--- docs/pambranding/scripts/help.js
+++ docs/pambranding/scripts/help.js
@@ -2,25 +2,24 @@
 // Help.js
 // =======================================================================================================
 $(document).ready(function() {
-	sel = $("P.MsoToc1 > a,P.MsoToc2 > a,P.MsoToc3 > a");
-	sel.attr('title', "Click to jump directly to this topic...");
-	sel.click(function() {
-		scrollToElement(this.hash.replace('#',''));
-	});
-	// Scroll the selected topic into view adjusting for the header bar.
-	function scrollToElement(elId) {
-		document.getElementsByName(elId)[0].scrollIntoView(true);
-		if  ($(window).height() + $(window).scrollTop() !== $(document).height())
-		{
-			$('html, body').animate({
-				scrollTop: "-=56"
-			}, "fast");
-		}
-	};
-	
-	$("#help_btnScrollToTop, .li2 > a").on("click", function() {
-		//window.scrollTo(0,380);
-		scrollToElement("TOC");
-	});
+    sel = $("P.MsoToc1 > a,P.MsoToc2 > a,P.MsoToc3 > a");
+    sel.attr('title', "Click to jump directly to this topic...");
+    sel.click(function() {
+        scrollToElement(this.hash.replace('#', ''));
+    });
+    // Scroll the selected topic into view adjusting for the header bar.
+    function scrollToElement(elId) {
+        document.getElementsByName(elId)[0].scrollIntoView(true);
+        if ($(window).height() + $(window).scrollTop() !== $(document).height()) {
+            $('html, body').animate({
+                scrollTop: "-=56"
+            }, "fast");
+        }
+    };
+
+    $("#help_btnScrollToTop, .li2 > a").on("click", function() {
+        //window.scrollTo(0,380);
+        scrollToElement("TOC");
+    });
 
 });
